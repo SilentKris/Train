615309bf41ba22058473c0eeaab97e9ff645f3fc
SilentKris <dwl130802305@gmail.com>
2020-08-19 23:24:40 -0700

    del

--- test.c
+++ test.c
@@ -3,7 +3,7 @@
 *   
 *   文件名称：test.c
 *   创 建 者:  dwl
-*   创建日期：2020/08/14
+*   创建日期：2020/08/19
 *   描    述：
 *
 *   更新日志：
@@ -13,10 +13,17 @@
 
 int main()
 {
-	int i;
-	for(i = 1; i<10;)
+	void *p = NULL;
+	union
 	{
-		printf("%d\n",i++);
-	}
+	int a = 10;
+	char b = "hello";
+	float c = 1.0;
+		
+	};
+
+	printf(" p = %d\n", sizeof(*p));
+	printf("*p = %d\n", *(int *)p);
+
 	return 0;
 }
